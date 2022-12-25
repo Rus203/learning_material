3e080f2c25a0ea851d3bd7b4435a62ee340bb5f6
Ruslan <c8man24@gmail.com>
2022-12-25 15:18:43 +0300

    feat: implemete sidebar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, ListItem, ListItemButton, ListItemIcon, ListItemText, List, Switch } from '@mui/material'
+import {
+  Home as HomeIcon, AutoStories as AutoStoriesIcon, Groups as GroupsIcon, Shop as ShopIcon,
+  Person as PersonIcon, Settings as SettingsIcon, AccountBox as ProfileIcon, ModeNight as ModeNightIcon
+}
+  from '@mui/icons-material'
 
 const SideBar = () => {
-  return <Box bgcolor="skyblue" p={2}>SideBar</Box>
+  return (
+    <Box p={2} flex={1}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <AutoStoriesIcon />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <ShopIcon />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <ProfileIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <ModeNightIcon />
+            </ListItemIcon>
+            <Switch color="default" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  )
 }
 
 export default SideBar
