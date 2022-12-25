69a318899b2624248bdc8a55a74ac06ce06816f6
Ruslan <c8man24@gmail.com>
2022-12-25 15:19:58 +0300

    refactor: adjust bloks

--- src/Components/RightBar.jsx
+++ src/Components/RightBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from '@mui/material'
 
 const RightBar = () => {
-  return <Box bgcolor="lightcoral" p={2}>RightBar</Box>
+  return <Box bgcolor="lightcoral" p={2} flex={2}>RightBar</Box>
 }
 
 export default RightBar
